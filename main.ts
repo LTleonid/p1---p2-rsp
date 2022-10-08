46fc4a269a25b32d4b44a3089c124aeabd5cb94f
LT_leonid <76744566+LTleonid@users.noreply.github.com>
2022-10-08 10:44:31 +0700

    Обновить pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
 function Start () {
+    Player2_Active = 0
+    Player1_Active = 0
     while (!(input.buttonIsPressed(Button.A))) {
         basic.showString("P1")
         if (input.buttonIsPressed(Button.A)) {
@@ -14,99 +16,91 @@ function Start () {
 }
 let Player2_choice = 0
 let Player2_Block = 0
-let Player2_Wait = 0
 let Player1_choice = 0
-let Player1_Block = 0
-let Player1_Wait = 0
 let Player1_Score = 0
 let Player2_Score = 0
 let Player1_Active = 0
 let Player2_Active = 0
-Player2_Active = 0
-Player1_Active = 0
+Start()
 basic.forever(function () {
     if (Player2_Active == 0 && Player1_Active == 0) {
         Player2_Score = 0
         Player1_Score = 0
         while (!(Player1_Score == 3 || Player2_Score == 3)) {
-            basic.showString("P1Choice")
-            Player1_Wait = 1
-            while (Player1_Wait == 1 && Player1_Block == 0) {
-                if (input.buttonIsPressed(Button.A)) {
-                    if (Player1_choice == 1) {
-                        basic.showLeds(`
-                            . . . . .
-                            . # # # .
-                            . # # # .
-                            . # # # .
-                            . . . . .
-                            `)
-                    } else if (Player1_choice == 2) {
-                        basic.showLeds(`
-                            # # . . #
-                            # # . # .
-                            . . # . .
-                            # # . # .
-                            # # . . #
-                            `)
-                    } else if (Player1_choice == 3) {
-                        basic.showLeds(`
-                            . . . . .
-                            . # # # #
-                            # # # # #
-                            # # # # .
-                            . . . . .
-                            `)
-                    } else if (Player1_choice > 3) {
-                        Player1_choice = 0
+            let Player1_Block = 0
+            if (Player1_Block == 0) {
+                basic.showString("P1Choice")
+                while (!(input.buttonIsPressed(Button.B))) {
+                    if (input.buttonIsPressed(Button.A)) {
+                        if (Player1_choice == 1) {
+                            basic.showLeds(`
+                                . . . . .
+                                . # # # .
+                                . # # # .
+                                . # # # .
+                                . . . . .
+                                `)
+                        } else if (Player1_choice == 2) {
+                            basic.showLeds(`
+                                # # . . #
+                                # # . # .
+                                . . # . .
+                                # # . # .
+                                # # . . #
+                                `)
+                        } else if (Player1_choice == 3) {
+                            basic.showLeds(`
+                                . . . . .
+                                . # # # #
+                                # # # # #
+                                # # # # .
+                                . . . . .
+                                `)
+                        } else if (Player1_choice > 3) {
+                            Player1_choice = 0
+                        }
+                        Player1_choice += 1
                     }
-                    Player1_choice += 1
-                }
-                if (input.buttonIsPressed(Button.B)) {
-                    Player1_Wait = 0
-                    Player1_Block = 0
                 }
             }
-            basic.showString("P2 Choice")
-            Player2_Wait = 1
-            while (Player2_Wait == 1 && Player2_Block == 0) {
-                if (input.buttonIsPressed(Button.A)) {
-                    if (Player2_choice == 1) {
-                        basic.showLeds(`
-                            . . . . .
-                            . # # # .
-                            . # # # .
-                            . # # # .
-                            . . . . .
-                            `)
-                    } else if (Player2_choice == 2) {
-                        basic.showLeds(`
-                            # # . . #
-                            # # . # .
-                            . . # . .
-                            # # . # .
-                            # # . . #
-                            `)
-                    } else if (Player2_choice == 3) {
-                        basic.showLeds(`
-                            . . . . .
-                            . # # # #
-                            # # # # #
-                            # # # # .
-                            . . . . .
-                            `)
-                    } else if (Player2_choice > 3) {
-                        Player2_choice = 0
+            if (Player2_Block == 0) {
+                basic.showString("P2Choice")
+                while (!(input.buttonIsPressed(Button.B))) {
+                    if (input.buttonIsPressed(Button.A)) {
+                        if (Player2_choice == 1) {
+                            basic.showLeds(`
+                                . . . . .
+                                . # # # .
+                                . # # # .
+                                . # # # .
+                                . . . . .
+                                `)
+                        } else if (Player2_choice == 2) {
+                            basic.showLeds(`
+                                # # . . #
+                                # # . # .
+                                . . # . .
+                                # # . # .
+                                # # . . #
+                                `)
+                        } else if (Player2_choice == 3) {
+                            basic.showLeds(`
+                                . . . . .
+                                . # # # #
+                                # # # # #
+                                # # # # .
+                                . . . . .
+                                `)
+                        } else if (Player2_choice > 3) {
+                            Player2_choice = 0
+                        }
+                        Player2_choice += 1
                     }
-                    Player2_choice += 1
-                }
-                if (input.buttonIsPressed(Button.B)) {
-                    Player2_Block = 1
-                    Player2_Wait = 0
+                    if (input.buttonIsPressed(Button.B)) {
+                        Player2_Block = 1
+                    }
                 }
             }
-            Player1_Wait = 1
-            Player2_Wait = 1
             if (Player2_choice == 1 && Player1_choice == 1) {
                 basic.showString("Tie")
             } else if (Player2_choice == 1 && Player1_choice == 2) {
@@ -132,8 +126,6 @@ basic.forever(function () {
             } else if (Player2_choice == 3 && Player1_choice == 3) {
                 basic.showString("Tie")
             }
-            Player2_Block = 0
-            Player1_Block = 0
         }
     }
 })
