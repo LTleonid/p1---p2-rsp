e4468bbb501a38fcfeada23e2c1e9105f35abc96
LT_leonid <76744566+LTleonid@users.noreply.github.com>
2024-11-22 22:36:49 +0700

    Обновить pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,133 +1,115 @@
-def Start():
-    global Player2_Active, Player1_Active
-    Player2_Active = 0
-    Player1_Active = 0
-    while not (input.button_is_pressed(Button.A)):
-        basic.show_string("P1")
-        if input.button_is_pressed(Button.A):
-            Player1_Active = 1
-    while not (input.button_is_pressed(Button.B)):
-        basic.show_string("P2")
-        if input.button_is_pressed(Button.B):
-            Player2_Active = 1
-Player2_choice = 0
-Player2_Wait = 0
+user = 0
 Player1_choice = 0
-Player1_Wait = 0
-Player1_Score = 0
 Player2_Score = 0
-Player1_Active = 0
-Player2_Active = 0
-Start()
+Player1_Score = 0
+Phase = 0
+Player2_Block = 0
+Player2_choice = 0
+def choice():
+    global user
+    user = 1
+    while input.button_is_pressed(Button.AB):
+        if input.button_is_pressed(Button.A):
+            if user != 0:
+                user += 0 - 1
+            else:
+                user = 3
+        if input.button_is_pressed(Button.B):
+            if user < 4:
+                user += 1
+            else:
+                user = 1
+        if Player1_choice == 1:
+            basic.show_leds("""
+                . . . . .
+                . # # # .
+                . # # # .
+                . # # # .
+                . . . . .
+                """)
+        elif Player1_choice == 2:
+            basic.show_leds("""
+                # # . . #
+                # # . # .
+                . . # . .
+                # # . # .
+                # # . . #
+                """)
+        elif Player1_choice == 3:
+            basic.show_leds("""
+                . . . . .
+                . # # # #
+                # # # # #
+                # # # # .
+                . . . . .
+                """)
+    return user
 
 def on_forever():
-    global Player2_Score, Player1_Score, Player1_Wait, Player1_choice, Player2_Wait, Player2_choice
-    if Player2_Active == 1 and Player1_Active == 1:
-        Player2_Score = 0
-        Player1_Score = 0
-        while not (Player1_Score == 3 or Player2_Score == 3):
-            basic.show_string("P1 Choice")
-            Player1_Wait = 1
-            while Player1_Wait == 1:
-                if input.button_is_pressed(Button.A):
-                    if Player1_choice == 1:
-                        basic.show_leds("""
-                            . . . . .
-                                                        . # # # .
-                                                        . # # # .
-                                                        . # # # .
-                                                        . . . . .
-                        """)
-                    elif Player1_choice == 2:
-                        basic.show_leds("""
-                            # # . . #
-                                                        # # . # .
-                                                        . . # . .
-                                                        # # . # .
-                                                        # # . . #
-                        """)
-                    elif Player1_choice == 3:
-                        basic.show_leds("""
-                            . . . . .
-                                                        . # # # #
-                                                        # # # # #
-                                                        # # # # .
-                                                        . . . . .
-                        """)
-                    elif Player1_choice > 3:
-                        Player1_choice = 1
-                    Player1_choice += 1
-                if input.button_is_pressed(Button.B):
-                    Player1_Wait = 0
-            basic.show_string("P2 Choice")
-            Player2_Wait = 1
-            while Player2_Wait == 1:
+    global Player2_Score, Player1_Score, Phase, Player2_choice, Player2_Block
+    Player2_Score = 0
+    Player1_Score = 0
+    while not (Player1_Score == 3 or Player2_Score == 3):
+        Phase = 0
+        if Phase == 0:
+            basic.show_string("P1Choice")
+            while not (input.button_is_pressed(Button.B)):
+                pass
+        if Player2_Block == 0:
+            basic.show_string("P2Choice")
+            while not (input.button_is_pressed(Button.B)):
                 if input.button_is_pressed(Button.A):
                     if Player2_choice == 1:
                         basic.show_leds("""
                             . . . . .
-                                                        . # # # .
-                                                        . # # # .
-                                                        . # # # .
-                                                        . . . . .
-                        """)
+                            . # # # .
+                            . # # # .
+                            . # # # .
+                            . . . . .
+                            """)
                     elif Player2_choice == 2:
                         basic.show_leds("""
                             # # . . #
-                                                        # # . # .
-                                                        . . # . .
-                                                        # # . # .
-                                                        # # . . #
-                        """)
+                            # # . # .
+                            . . # . .
+                            # # . # .
+                            # # . . #
+                            """)
                     elif Player2_choice == 3:
                         basic.show_leds("""
                             . . . . .
-                                                        . # # # #
-                                                        # # # # #
-                                                        # # # # .
-                                                        . . . . .
-                        """)
+                            . # # # #
+                            # # # # #
+                            # # # # .
+                            . . . . .
+                            """)
                     elif Player2_choice > 3:
-                        Player2_choice = 1
+                        Player2_choice = 0
                     Player2_choice += 1
                 if input.button_is_pressed(Button.B):
-                    Player2_Wait = 0
+                    Player2_Block = 1
+        if Player2_choice == 1 and Player1_choice == 1:
+            basic.show_string("Tie")
+        elif Player2_choice == 1 and Player1_choice == 2:
+            basic.show_string("P1 Win")
+            Player1_Score += 1
+        elif Player2_choice == 1 and Player1_choice == 3:
+            basic.show_string("P2 Win")
+            Player2_Score += 1
+        elif Player2_choice == 2 and Player1_choice == 1:
+            basic.show_string("P1 Win")
+            Player1_Score += 1
+        elif Player2_choice == 2 and Player1_choice == 2:
+            basic.show_string("Tie")
+        elif Player2_choice == 2 and Player1_choice == 3:
+            basic.show_string("P2 Win")
+            Player2_Score += 1
+        elif Player2_choice == 3 and Player1_choice == 1:
+            basic.show_string("P1 Win")
+            Player1_Score += 1
+        elif Player2_choice == 3 and Player1_choice == 2:
+            basic.show_string("P2 Win")
+            Player2_Score += 1
+        elif Player2_choice == 3 and Player1_choice == 3:
+            basic.show_string("Tie")
 basic.forever(on_forever)
-
-# 1 и 1 - ничья
-# 1 и 2 - 1
-# 1 и 3 - 3
-# 2 и 1 - 1
-# 2 и 2 - ничья
-# 2 и 3 - 2
-# 3 и 1 - 3
-# 3 и 2 - 2
-# 3 и 3 - ничья
-
-def on_forever2():
-    global Player1_Score, Player2_Score
-    if Player2_choice == 1 and Player1_choice == 1:
-        basic.show_string("Tie")
-    elif Player2_choice == 1 and Player1_choice == 2:
-        basic.show_string("P1 Win")
-        Player1_Score += 1
-    elif Player2_choice == 1 and Player1_choice == 3:
-        basic.show_string("P2 Win")
-        Player2_Score += 1
-    elif Player2_choice == 2 and Player1_choice == 1:
-        basic.show_string("P1 Win")
-        Player1_Score += 1
-    elif Player2_choice == 2 and Player1_choice == 2:
-        basic.show_string("Tie")
-    elif Player2_choice == 2 and Player1_choice == 3:
-        basic.show_string("P2 Win")
-        Player2_Score += 1
-    elif Player2_choice == 3 and Player1_choice == 1:
-        basic.show_string("P1 Win")
-        Player1_Score += 1
-    elif Player2_choice == 3 and Player1_choice == 2:
-        basic.show_string("P2 Win")
-        Player2_Score += 1
-    elif Player2_choice == 3 and Player1_choice == 3:
-        basic.show_string("Tie")
-basic.forever(on_forever2)
--- main.ts
+++ main.ts
@@ -1,131 +1,87 @@
-function Start () {
-    Player2_Active = 0
-    Player1_Active = 0
-    while (!(input.buttonIsPressed(Button.A))) {
-        basic.showString("P1")
+let user = 0
+let Phase = 0
+let Player1_choice = 0
+let Player2_choice = 0
+function choice () {
+    user = 1
+    while (!(input.buttonIsPressed(Button.AB))) {
         if (input.buttonIsPressed(Button.A)) {
-            Player1_Active = 1
+            if (user != 0) {
+                user += 0 - 1
+            } else {
+                user = 3
+            }
+        }
+        if (input.buttonIsPressed(Button.B)) {
+            if (user < 4) {
+                user += 1
+            } else {
+                user = 1
+            }
+        }
+        if (user == 1) {
+            basic.showLeds(`
+                . . . . .
+                . # # # .
+                . # # # .
+                . # # # .
+                . . . . .
+                `)
+        } else if (user == 2) {
+            basic.showLeds(`
+                # # . . #
+                # # . # .
+                . . # . .
+                # # . # .
+                # # . . #
+                `)
+        } else if (user == 3) {
+            basic.showLeds(`
+                . . . . .
+                . # # # #
+                # # # # #
+                # # # # .
+                . . . . .
+                `)
         }
     }
-    while (!(input.buttonIsPressed(Button.B))) {
-        basic.showString("P2")
-        if (input.buttonIsPressed(Button.B)) {
-            Player2_Active = 1
-        }
+    return user
+}
+function Compare (num: number, num2: number) {
+    if (num == 1 && num2 == 1) {
+        basic.showString("Tie")
+    } else if (num == 1 && num2 == 2) {
+        basic.showString("P1 Win")
+    } else if (num == 1 && num2 == 3) {
+        basic.showString("P2 Win")
+    } else if (num == 2 && num2 == 1) {
+        basic.showString("P1 Win")
+    } else if (num == 2 && num2 == 2) {
+        basic.showString("Tie")
+    } else if (num == 2 && num2 == 3) {
+        basic.showString("P2 Win")
+    } else if (num == 3 && num2 == 1) {
+        basic.showString("P1 Win")
+    } else if (num == 3 && num2 == 2) {
+        basic.showString("P2 Win")
+    } else if (num == 3 && num2 == 3) {
+        basic.showString("Tie")
     }
 }
-let Player2_choice = 0
-let Player2_Block = 0
-let Player1_choice = 0
-let Player1_Score = 0
-let Player2_Score = 0
-let Player1_Active = 0
-let Player2_Active = 0
-Start()
 basic.forever(function () {
-    if (Player2_Active == 0 && Player1_Active == 0) {
-        Player2_Score = 0
-        Player1_Score = 0
-        while (!(Player1_Score == 3 || Player2_Score == 3)) {
-            let Player1_Block = 0
-            if (Player1_Block == 0) {
-                basic.showString("P1Choice")
-                while (!(input.buttonIsPressed(Button.B))) {
-                    if (input.buttonIsPressed(Button.A)) {
-                        if (Player1_choice == 1) {
-                            basic.showLeds(`
-                                . . . . .
-                                . # # # .
-                                . # # # .
-                                . # # # .
-                                . . . . .
-                                `)
-                        } else if (Player1_choice == 2) {
-                            basic.showLeds(`
-                                # # . . #
-                                # # . # .
-                                . . # . .
-                                # # . # .
-                                # # . . #
-                                `)
-                        } else if (Player1_choice == 3) {
-                            basic.showLeds(`
-                                . . . . .
-                                . # # # #
-                                # # # # #
-                                # # # # .
-                                . . . . .
-                                `)
-                        } else if (Player1_choice > 3) {
-                            Player1_choice = 0
-                        }
-                        Player1_choice += 1
-                    }
-                }
-            }
-            if (Player2_Block == 0) {
-                basic.showString("P2Choice")
-                while (!(input.buttonIsPressed(Button.B))) {
-                    if (input.buttonIsPressed(Button.A)) {
-                        if (Player2_choice == 1) {
-                            basic.showLeds(`
-                                . . . . .
-                                . # # # .
-                                . # # # .
-                                . # # # .
-                                . . . . .
-                                `)
-                        } else if (Player2_choice == 2) {
-                            basic.showLeds(`
-                                # # . . #
-                                # # . # .
-                                . . # . .
-                                # # . # .
-                                # # . . #
-                                `)
-                        } else if (Player2_choice == 3) {
-                            basic.showLeds(`
-                                . . . . .
-                                . # # # #
-                                # # # # #
-                                # # # # .
-                                . . . . .
-                                `)
-                        } else if (Player2_choice > 3) {
-                            Player2_choice = 0
-                        }
-                        Player2_choice += 1
-                    }
-                    if (input.buttonIsPressed(Button.B)) {
-                        Player2_Block = 1
-                    }
-                }
-            }
-            if (Player2_choice == 1 && Player1_choice == 1) {
-                basic.showString("Tie")
-            } else if (Player2_choice == 1 && Player1_choice == 2) {
-                basic.showString("P1 Win")
-                Player1_Score += 1
-            } else if (Player2_choice == 1 && Player1_choice == 3) {
-                basic.showString("P2 Win")
-                Player2_Score += 1
-            } else if (Player2_choice == 2 && Player1_choice == 1) {
-                basic.showString("P1 Win")
-                Player1_Score += 1
-            } else if (Player2_choice == 2 && Player1_choice == 2) {
-                basic.showString("Tie")
-            } else if (Player2_choice == 2 && Player1_choice == 3) {
-                basic.showString("P2 Win")
-                Player2_Score += 1
-            } else if (Player2_choice == 3 && Player1_choice == 1) {
-                basic.showString("P1 Win")
-                Player1_Score += 1
-            } else if (Player2_choice == 3 && Player1_choice == 2) {
-                basic.showString("P2 Win")
-                Player2_Score += 1
-            } else if (Player2_choice == 3 && Player1_choice == 3) {
-                basic.showString("Tie")
-            }
-        }
+    Phase = 0
+    if (Phase == 0) {
+        basic.showString("P1")
+        Player1_choice = choice()
+        Phase += 1
+    }
+    if (Phase == 1) {
+        basic.showString("P2")
+        Player2_choice = choice()
+        Phase += 1
+    }
+    if (Phase == 2) {
+        Compare(Player2_choice, Player1_choice)
+        Phase = 0
     }
 })
